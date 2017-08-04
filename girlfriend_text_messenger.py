954d8cf338dcbb25c87e53df33223011e4defb12
NBurnettt <burnettn@onid.oregonstate.edu>
2017-08-03 23:58:20 -0700

    Only firing script accesses secrets now

--- girlfriend_text_messenger.py
+++ girlfriend_text_messenger.py
@@ -1,14 +1,14 @@
 from twilio_text_messenger import TwilioTextMessenger
-from secrets import girlfriend_name, city
 from weather_grabber import WeatherGrabber
 from compliment_generator import ComplimentGenerator
 
 
 class GirlfriendTextMessenger(TwilioTextMessenger):
-    def __init__(self):
-        super().__init__()
-        self.girlfriend_name = girlfriend_name
-        self.girlfriend_city = city
+    def __init__(self, **kwargs):
+        self.girlfriend_name = kwargs.pop('girlfriend_name')
+        self.girlfriend_city = kwargs.pop('city')
+        self.apixu_api_key = kwargs.pop('apixu_api_key')
+        super().__init__(**kwargs)
 
     def send_girlfriend_text(self):
         name = "My_Cell"
@@ -18,7 +18,7 @@ class GirlfriendTextMessenger(TwilioTextMessenger):
         self.send(name, text_body)
 
     def weather_update(self):
-        high, low, average, description = WeatherGrabber(self.girlfriend_city).get_today()
+        high, low, average, description = WeatherGrabber(self.apixu_api_key, self.girlfriend_city).get_today()
         update = '\rP.S. Today will be {} and about {} degrees (high of {}, low of {}).'\
             .format(description, average, high, low)
         return update
