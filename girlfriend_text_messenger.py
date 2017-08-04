95cc5207f3795cbdb3b2e78da6b735ed6100b7d3
NBurnettt <burnettn@onid.oregonstate.edu>
2017-08-03 23:32:49 -0700

    Gave code better OO approach

--- girlfriend_text_messenger.py
+++ girlfriend_text_messenger.py
@@ -1,26 +1,24 @@
 from twilio_text_messenger import TwilioTextMessenger
-from secrets import girlfriend_name
-import random
-import requests
+from secrets import girlfriend_name, city
+from weather_grabber import WeatherGrabber
+from compliment_generator import ComplimentGenerator
 
 
 class GirlfriendTextMessenger(TwilioTextMessenger):
     def __init__(self):
         super().__init__()
         self.girlfriend_name = girlfriend_name
+        self.girlfriend_city = city
 
     def send_girlfriend_text(self):
         name = "My_Cell"
-        random.seed(None)
-        x = random.randint(1,18)
-        comps = open("compliments.txt", "r")
-        for i in range(x):
-            compliment = comps.readline()
-        text_body = '-\r\rGood Morning {}!\r' + name
-        r = requests.get('https://query.yahooapis.com/v1/public/yql?q=select%20item.forecast%20from%20weather.forecast%20where%20woeid%20in%20(2385382)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
-        data = r.json()
-        high = data['query']['results']['channel'][0]['item']['forecast']['high']
-        low = data['query']['results']['channel'][0]['item']['forecast']['low']
-        details = data['query']['results']['channel'][0]['item']['forecast']['text']
-        text_body += "\rP.S. Today will be " + details + " with a high of " + high + " and a low of " + low + ". "
+        compliment = ComplimentGenerator.get_girlfriend_compliment()
+        text_body = '-\r\rGood Morning {}!\r'.format(self.girlfriend_name) + compliment
+        text_body += self.weather_update()
         self.send(name, text_body)
+
+    def weather_update(self):
+        high, low, average, description = WeatherGrabber(self.girlfriend_city).get_today()
+        update = '\rP.S. Today will be {} and about {} degrees (high of {}, low of {}).'\
+            .format(description, average, high, low)
+        return update
